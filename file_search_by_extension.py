8f2d3595004c17b4d3459c1cc881f33d3ff07a54
Sandmann11 <sandmann@o2.pl>
2017-05-12 00:07:05 +0200

    Added on May 12

---
+++ file_search_by_extension.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import fnmatch
+import os
+import shutil
+
+results = []
+for root, dirs, files in os.walk("C:\\"):
+    for _file in files:
+        if fnmatch.fnmatch(_file, '*.docx'):
+            results.append(os.path.join(root, _file))
+
+for fl in results:
+    shutil.copy2(fl, "C:\\test_dir")
+
+print(results)
